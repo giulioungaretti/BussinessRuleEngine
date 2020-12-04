8c819b621a3dd7f332cd0092c1be38ebe6091f84
Giulio Ungaretti <unga@Giulios-MacBook-Pro.local>
2020-12-04 23:06:11 +0100

    Add test for all the spec

--- Test/Tests.fs
+++ Test/Tests.fs
@@ -3,8 +3,6 @@ module Tests
 open Domain
 open Xunit
 
-
-
 let customerInfo =
     { Email = "foo@bar.com"
       Adress = "street foo bar" }
@@ -32,6 +30,7 @@ let ``Test physical product`` () =
     Assert.Equal<Result list>(expected, result)
 
 
+
 [<Fact>]
 let ``Test Book `` () =
     // • If the payment is for a physical product
@@ -40,9 +39,22 @@ let ``Test Book `` () =
     // • create a duplicate packing slip for the royalty department.
     // • If the payment is for a physical product or a book,
     // generate a commission payment to the agent.
-    ()
 
 
+    let order = Phyiscal Book
+
+    let result = Engine.ProcessOrder(order)
+
+    let packingSlip =
+        { Adress = customerInfo.Adress
+          Extra = None }
+
+    let expected =
+        [ ShipProdcut(packingSlip)
+          NotifyRoyality(packingSlip)
+          PaymentCommission ]
+
+    Assert.Equal<Result list>(expected, result)
 
 [<Fact>]
 let ``Test Video `` () =
@@ -53,9 +65,22 @@ let ``Test Video `` () =
     // • the result of a court decision in 1997
     // • If the payment is for a physical product or a book,
     // generate a commission payment to the agent.
-    ()
 
+    let order = Phyiscal Video
 
+    let result = Engine.ProcessOrder(order)
+
+    let packingSlip =
+        { Adress = customerInfo.Adress
+          Extra = None }
+
+    let expected =
+        [ ShipProdcut
+            ({ packingSlip with
+                   Extra = Some(Phyiscal(Other "First Aid kit")) })
+          PaymentCommission ]
+
+    Assert.Equal<Result list>(expected, result)
 
 
 [<Fact>]
@@ -65,7 +90,15 @@ let ``Test Membership `` () =
     // • If the payment is for a membership or upgrade
     //  e-mail the owner and inform them of the activation/upgrade.
 
-    ()
+    let order = Digital NewMember
+
+    let result = Engine.ProcessOrder(order)
+
+    let expected =
+        [ ActivateMemberShip
+          NotifyMeber memberInfo ]
+
+    Assert.Equal<Result list>(expected, result)
 
 [<Fact>]
 let ``Test Membership  Update`` () =
@@ -73,4 +106,13 @@ let ``Test Membership  Update`` () =
     // •  apply the upgrade.
     // • If the payment is for a membership or upgrade
     //  e-mail the owner and inform them of the activation/upgrade.
-    ()
+
+    let order = Digital UpgradeMember
+
+    let result = Engine.ProcessOrder(order)
+
+    let expected =
+        [ UpgradeMemberShip
+          NotifyMeber memberInfo ]
+
+    Assert.Equal<Result list>(expected, result)
