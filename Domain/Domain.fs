1b05b5920f3726a00817240f64c4437e7aa50739
Giulio Ungaretti <unga@Giulios-MacBook-Pro.local>
2020-12-04 23:02:26 +0100

    Flesh out a simple domain

--- Domain/Domain.fs
+++ Domain/Domain.fs
@@ -1,2 +1,33 @@
 namespace Domain
 
+type DigitalProduct =
+    | NewMember
+    | UpgradeMember
+
+type PhyiscalProduct =
+    | Book
+    | Video
+    | Other of string
+
+type Product =
+    | Phyiscal of PhyiscalProduct
+    | Digital of DigitalProduct
+
+type PackingSlip =
+    { Adress: string
+      Extra: Option<Product> }
+
+type MemberShipInfo = { Email: string }
+
+type Result =
+    | ShipProdcut of PackingSlip
+    | NotifyRoyality of PackingSlip
+    | NotifyMeber of MemberShipInfo
+    | ActivateMemberShip
+    | UpgradeMemberShip
+    | PaymentCommission
+    | AddFirstAidKit
+    | NoOp of string
+
+
+type CustomerInfo = { Email: string; Adress: string }
