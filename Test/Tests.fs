7b45b21d627554ceb97358c0e0e8d7f0f1122146
Giulio Ungaretti <unga@Giulios-MacBook-Pro.local>
2020-12-04 23:00:01 +0100

    Transform spec into test plan

--- Test/Tests.fs
+++ Test/Tests.fs
@@ -1,8 +1,56 @@
 module Tests
 
-open System
+open Domain
 open Xunit
 
+
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Test physical product`` () =
+    // • If the payment is for a physical product
+    //  generate a packing slip for shipping.
+    // • If the payment is for a physical product or a book,
+    // • generate a commission payment to the agent.
+    ()
+
+
+[<Fact>]
+let ``Test Book `` () =
+    // • If the payment is for a physical product
+    // •  generate a packing slip for shipping.
+    // • If the payment is for a book
+    // • create a duplicate packing slip for the royalty department.
+    // • If the payment is for a physical product or a book,
+    // generate a commission payment to the agent.
+    ()
+
+
+[<Fact>]
+let ``Test Video `` () =
+    // • If the payment is for a physical product
+    // •  generate a packing slip for shipping.
+    // • If the payment is for the video “Learning to Ski,
+    // •  add a free “First Aid” video to the packing slip
+    // • the result of a court decision in 1997
+    // • If the payment is for a physical product or a book,
+    // generate a commission payment to the agent.
+    ()
+
+
+
+
+[<Fact>]
+let ``Test Membership `` () =
+    // • If the payment is for a membership
+    // activate that membership.
+    // • If the payment is for a membership or upgrade
+    //  e-mail the owner and inform them of the activation/upgrade.
+
+    ()
+
+[<Fact>]
+let ``Test Membership  Update`` () =
+    // • If the payment is an upgrade to a membership
+    // •  apply the upgrade.
+    // • If the payment is for a membership or upgrade
+    //  e-mail the owner and inform them of the activation/upgrade.
+    ()
